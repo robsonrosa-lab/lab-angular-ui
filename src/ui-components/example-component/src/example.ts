1dfbb76b25b813edeba72db55c4f6cc1abf0b243
robsonrosa <robsonrosa@cwi.com.br>
2016-08-11 17:26:12 -0300

    Ending exposing modules tests

---
+++ src/ui-components/example-component/src/example.ts
@@ -0,0 +1,5 @@
+import ui = require('lab-angular-ui-ui');
+import Fruit = ui.Fruit;
+
+var a = new Fruit(1);
+
---
+++ src/ui/src/ui.ts
@@ -0,0 +1,12 @@
+import core = require('lab-angular-ui-core');
+import IEntity = core.Entity;
+
+export class Fruit extends core.Entity {
+
+}
+
+
+
+
+
+
